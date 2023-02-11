11e28c2293307af9f77c37a3b35747c0f180d860
Erit Islami <eritislami@gmail.com>
2023-02-11 18:14:31 +0100

    📈 Add vercel analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-
+import { Analytics } from '@vercel/analytics/react'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
@@ -28,6 +28,7 @@ export default function App({ Component, pageProps, router }) {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </>
   )
 }
